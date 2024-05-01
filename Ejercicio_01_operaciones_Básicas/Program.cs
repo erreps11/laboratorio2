544df17465dca1400967932697dcb83a01e7d462
erreps11 <167550817+erreps11@users.noreply.github.com>
2024-05-01 13:35:44 -0500

    cambio 1, ejercicio 1 desarrollado.

---
+++ Ejercicio_01_operaciones_Básicas/Program.cs
@@ -0,0 +1,31 @@
+﻿// Realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario. 
+
+double numero1, numero2;
+
+Console.Write("ingresa el primer numero: ");
+while(!double.TryParse(Console.ReadLine(), out numero1))
+{
+    Console.Write("Número no válido. Ingrese un número válido");
+}
+
+Console.Write("ingresa el primer numero: ");
+while (!double.TryParse(Console.ReadLine(), out numero2))
+{
+    Console.Write("Número no válido. Ingrese un número válido");
+}
+
+Console.WriteLine($"Suma: {numero1 + numero2}");
+Console.WriteLine($"Resta: {numero1 - numero2}");
+Console.WriteLine($"Producto: {numero1 * numero2}");
+
+// Para la division; verificar que el divisor sea diferente de cero:
+
+if (numero2 != 0)
+{
+
+    Console.WriteLine($"División: {numero1 / numero2 }");
+}
+else
+{
+    Console.WriteLine("No se puede dividir entre cero. ");
+}
